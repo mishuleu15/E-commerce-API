a28405758e8122c31e88e036f70a129a8fbe89d2
unknown <mishu_leu15@yahoo.com>
2021-12-14 21:40:57 +0200

    first commit/ created express server, added connection to MongoDB, created User model, added .env file, added morgan middleware

---
+++ app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+require('express-async-errors');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const morgan = require('morgan');
+
+// express
+const express = require('express');
+const app = express();
+
+// database
+const connectDB = require('./db/connect');
+
+//middleware
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('GET request to the homepage');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const PORT = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
