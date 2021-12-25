869ae977ba4a1864c6bf9b1e6bb2349d5991f9ce
unknown <mishu_leu15@yahoo.com>
2021-12-25 21:12:54 +0200

    added authentication and authorization middlewares, update password and user controllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 const jwt = require('jsonwebtoken');
-const { createJWT, attachCookiesToResponse } = require('../utils');
+const { createTokenUser, attachCookiesToResponse } = require('../utils');
 
 const register = async (req, res) => {
   const { email, name, password, role } = req.body;
@@ -43,8 +43,9 @@ const login = async (req, res) => {
     throw new CustomError.UnauthenticatedError('Invalid Credentials');
   }
 
-  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  // const tokenUser = { name: user.name, userId: user._id, role: user.role };
 
+  const tokenUser = createTokenUser(user);
   attachCookiesToResponse({ res, user: tokenUser });
 
   res.status(StatusCodes.OK).json({ user: tokenUser });
--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,16 +1,34 @@
 const CustomError = require('../errors');
 const { isTokenValid } = require('../utils');
 
-const authenticateUser = (req, res, next) => {
+const authenticateUser = async (req, res, next) => {
   const token = req.signedCookies.token;
+
   if (!token) {
-    console.log('error, no token present');
-  } else {
-    console.log('token present');
+    throw new CustomError.UnauthenticatedError('Authentication Invalid');
   }
-  next();
+
+  try {
+    const { name, userId, role } = isTokenValid({ token });
+    req.user = { name, userId, role };
+    next();
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError('Authentication Invalid');
+  }
+};
+
+const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError(
+        'Unauthorized to access this route'
+      );
+    }
+    next();
+  };
 };
 
 module.exports = {
   authenticateUser,
+  authorizePermissions,
 };
