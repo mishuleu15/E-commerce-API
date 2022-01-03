20402e8f12a0c704dd20959de707534555d19ead
unknown <mishu_leu15@yahoo.com>
2022-01-03 19:14:35 +0200

    added aggregate pipeline to review model, created order model and order controller and routesclear

---
+++ models/temp.js
@@ -0,0 +1,36 @@
+/*
+ * Requires the MongoDB Node.js Driver
+ * https://mongodb.github.io/node-mongodb-native
+ */
+
+const agg = [
+  {
+    $match: {
+      product: new ObjectId('61d2d358f92616f25bb1b21e'),
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      averageRating: {
+        $avg: '$rating',
+      },
+      numberOfReviews: {
+        $sum: 1,
+      },
+    },
+  },
+];
+
+MongoClient.connect(
+  '',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (connectErr, client) {
+    assert.equal(null, connectErr);
+    const coll = client.db('').collection('');
+    coll.aggregate(agg, (cmdErr, result) => {
+      assert.equal(null, cmdErr);
+    });
+    client.close();
+  }
+);
